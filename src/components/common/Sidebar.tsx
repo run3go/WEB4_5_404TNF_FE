2a8004f4d1efee28906ea2cc23bc8b337ec5556a
ty <composedkim@naver.com>
2025-07-07 00:43:59 +0900

    💄Style: sidebar 아이콘 추가

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,33 +1,41 @@
 import Image from 'next/image';
 import logo from '@/assets/images/logo-main1.png';
+import Icon from './Icon';
 
 export default function Sidebar() {
   return (
     <>
-      <div className="relative flex h-[100vh] w-[220px] flex-col items-center bg-[#FFFDF7] p-8 text-[#2B2926]">
+      <div className="relative my-12 ml-14 flex h-[cal(100vh-48px)] w-[220px] flex-col items-center bg-[#FFFDF7] p-8 text-[#2B2926]">
         <Image src={logo} alt="댕댕일지 로고" />
         <div className="mt-10 ml-2 space-y-6 text-[16px] font-medium">
-          <div className="flex h-[40px] w-[180px] cursor-pointer items-center justify-center rounded-[15px] hover:bg-[#FFDBAB]">
+          <div className="sidebar__content">
+            <Icon width="28px" height="28px" left="-22px" top="-21px" />
             <p>대시보드</p>
           </div>
-          <div className="flex h-[40px] w-[180px] cursor-pointer items-center justify-center rounded-[15px] hover:bg-[#FFDBAB]">
+          <div className="sidebar__content">
+            <Icon width="28px" height="28px" left="-62px" top="-19px" />
             <p>일정</p>
           </div>
-          <div className="flex h-[40px] w-[180px] cursor-pointer items-center justify-center rounded-[15px] hover:bg-[#FFDBAB]">
+          <div className="sidebar__content">
+            <Icon width="28px" height="28px" left="-102px" top="-17px" />
             <p>댕댕일지</p>
           </div>
-          <div className="flex h-[40px] w-[180px] cursor-pointer items-center justify-center rounded-[15px] hover:bg-[#FFDBAB]">
+          <div className="sidebar__content">
+            <Icon width="28px" height="28px" left="-144px" top="-19px" />
             <p>게시판</p>
           </div>
-          <div className="flex h-[40px] w-[180px] cursor-pointer items-center justify-center rounded-[15px] hover:bg-[#FFDBAB]">
+          <div className="sidebar__content">
+            <Icon width="28px" height="28px" left="-184px" top="-19px" />
             <p>케어가이드</p>
           </div>
         </div>
         <div className="absolute bottom-10 space-y-6">
-          <div className="flex h-[40px] w-[180px] cursor-pointer items-center justify-center rounded-[15px] hover:bg-[#FFDBAB]">
+          <div className="sidebar__content">
+            <Icon width="28px" height="28px" left="-224px" top="-18px" />
             <p>다크모드</p>
           </div>
-          <div className="flex h-[40px] w-[180px] cursor-pointer items-center justify-center rounded-[15px] hover:bg-[#FFDBAB]">
+          <div className="sidebar__content">
+            <Icon width="28px" height="28px" left="-264px" top="-18px" />
             <p>로그아웃</p>
           </div>
         </div>
