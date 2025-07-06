9cf23c485ab3cb49ebe24f0c28ab56fff03b9579
run3go <qkrwjdtn09@gmail.com>
2025-07-06 17:15:00 +0900

    💄 Style: 셀렉트 박스 퍼블리싱

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import TodoCard from '@/components/dashboard/TodoCard';
 import WalkCard from '@/components/dashboard/WalkCard';
 
 import speechBubble from '@/assets/images/speech-bubble.png';
-import Button from '@/components/common/Button';
+import SelectBox from '@/components/common/SelectBox';
 import Image from 'next/image';
 
 export default async function Dashboard() {
@@ -30,18 +30,21 @@ export default async function Dashboard() {
     { date: '2025-07-07', value: 1.0 },
   ];
 
+  const options = [
+    { value: '이마음', label: '이마음' },
+    { value: '이구름', label: '이구름' },
+    { value: '이솜', label: '이솜' },
+  ];
+
   return (
     <>
       <div className="w-[1548px] bg-[var(--background)] p-12">
         <div className="mb-13 flex items-center justify-between">
-          <Button>수정하기</Button>
           <h2 className="text-[22px] font-bold">
             마음이는 평균보다 3kg 무거워요! 다이어트가 필요하니 간식을 줄여보는
             건 어떨까요?
           </h2>
-          <select name="dog" id="dog">
-            <option value="이마음">이마음</option>
-          </select>
+          <SelectBox options={options} width={110} footstep />
         </div>
         <div className="flex gap-12">
           <div className="flex flex-col gap-12">
