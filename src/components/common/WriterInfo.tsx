fa5923fa424034b6a1c9e9aa6b390697bbac9648
ty <composedkim@naver.com>
2025-07-07 02:14:25 +0900

    💄Style: 작성자 정보 컴포넌트 퍼블리싱

---
+++ src/components/common/WriterInfo.tsx
@@ -0,0 +1,18 @@
+interface WriterInfo {
+  name: string;
+  postedAt: string;
+}
+
+export default function WriterInfo({ name, postedAt }: WriterInfo) {
+  return (
+    <>
+      <div className="flex items-center gap-4">
+        <div className="h-[42px] w-[42px] rounded-full bg-gray-500"></div>
+        <div className="text-[14px] font-medium">
+          <p>{name}</p>
+          <p className="text-[#909090]">{postedAt}</p>
+        </div>
+      </div>
+    </>
+  );
+}
