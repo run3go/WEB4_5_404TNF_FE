6e3a843cce8cf57b226564caf748c9ec307e813a
ty <composedkim@naver.com>
2025-07-07 15:38:19 +0900

    💄Style: WriterInfo 크기 받아서 구별

--- src/components/common/WriterInfo.tsx
+++ src/components/common/WriterInfo.tsx
@@ -1,14 +1,23 @@
 interface WriterInfo {
   name: string;
   postedAt: string;
+  size?: 'small' | 'big';
 }
 
-export default function WriterInfo({ name, postedAt }: WriterInfo) {
+export default function WriterInfo({
+  name,
+  postedAt,
+  size = 'small',
+}: WriterInfo) {
+  const isBig = size === 'big';
+  const avatarSize = isBig ? 'w-[52px] h-[52px]' : 'w-[42px] h-[42px]';
+  const textSize = isBig ? 'text-[16px] space-y-1' : 'text-[14px]';
+
   return (
     <>
       <div className="flex w-fit cursor-pointer items-center gap-4">
-        <div className="h-[42px] w-[42px] rounded-full bg-gray-500"></div>
-        <div className="text-[14px] font-medium">
+        <div className={`rounded-full bg-gray-500 ${avatarSize}`}></div>
+        <div className={`font-medium ${textSize}`}>
           <p>{name}</p>
           <p className="text-[#909090]">{postedAt}</p>
         </div>
