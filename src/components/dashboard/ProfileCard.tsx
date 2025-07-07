6d21d3e4d6713b58c8defe63dd83ff50760cefa2
run3go <qkrwjdtn09@gmail.com>
2025-07-07 22:08:09 +0900

    💄 Style: 커스텀 체크박스 스타일

--- src/components/dashboard/ProfileCard.tsx
+++ src/components/dashboard/ProfileCard.tsx
@@ -1,14 +1,22 @@
 import dog from '@/assets/images/dog_img.png';
 import Image from 'next/image';
 import Card from '../common/Card';
+import Icon from '../common/Icon';
 
 export default function ProfileCard() {
   return (
     <Card className="flex w-full max-w-[560px] flex-row-reverse justify-end sm:flex-row sm:justify-between">
       <ul className="flex flex-col gap-3 text-sm font-medium sm:gap-[18px] sm:text-xl">
-        <li>
+        <li className="flex items-center">
           <span className="mr-3 text-[var(--color-grey)]">이름</span>
           <span>이마음</span>
+          <Icon
+            className="ml-2"
+            width="12px"
+            height="19px"
+            left="-110px"
+            top="-79px"
+          />
         </li>
         <li>
           <span className="mr-3 text-[var(--color-grey)]">견종</span>
@@ -19,7 +27,13 @@ export default function ProfileCard() {
           <span>5년 6개월</span>
         </li>
         <li>
-          <span>가족이 된지 1943일</span>
+          <span>
+            가족이 된지{' '}
+            <strong className="font-medium text-[var(--color-primary-500)]">
+              1943
+            </strong>
+            일
+          </span>
         </li>
       </ul>
       <Image
--- src/components/dashboard/TodoCard.tsx
+++ src/components/dashboard/TodoCard.tsx
@@ -1,26 +1,60 @@
 import Card from '../common/Card';
+import Icon from '../common/Icon';
 
 export default function TodoCard() {
+  const checked = false;
   return (
     <Card className="flex aspect-[255/322] w-full max-w-[255px] flex-col">
       <h2 className="mb-5 text-xs font-medium sm:text-base">오늘의 할 일</h2>
       <ul className="scrollbar-hidden flex grow-1 flex-col gap-3 overflow-y-scroll text-sm select-none sm:text-base">
         <li>
-          <input className="mr-[13px] cursor-pointer" id="1" type="checkbox" />
-          <label className="cursor-pointer" htmlFor="1">
-            잘 먹기
+          <label
+            className="flex cursor-pointer items-center gap-2"
+            htmlFor="todo1"
+          >
+            <Icon
+              width="22px"
+              height="22px"
+              left={checked ? '-100px' : '-146px'}
+              top="-254px"
+            />
+            <input
+              className="mr-[13px] cursor-pointer"
+              hidden
+              id="todo1"
+              type="checkbox"
+            />
+            <span className="leading-[1.2]">잘 먹기</span>
           </label>
         </li>
         <li>
-          <input className="mr-[13px] cursor-pointer" id="2" type="checkbox" />
-          <label className="cursor-pointer" htmlFor="2">
-            잘 자기
+          <label
+            className="flex cursor-pointer items-center gap-2"
+            htmlFor="todo2"
+          >
+            <Icon width="22px" height="22px" left="-100px" top="-254px" />
+            <input
+              className="mr-[13px] cursor-pointer"
+              hidden
+              id="todo2"
+              type="checkbox"
+            />
+            <span className="leading-[1.2]">잘 자기</span>
           </label>
         </li>
         <li>
-          <input className="mr-[13px] cursor-pointer" id="3" type="checkbox" />
-          <label className="cursor-pointer" htmlFor="3">
-            끝내주게 놀기
+          <label
+            className="flex cursor-pointer items-center gap-2"
+            htmlFor="todo3"
+          >
+            <Icon width="22px" height="22px" left="-100px" top="-254px" />
+            <input
+              className="mr-[13px] cursor-pointer"
+              hidden
+              id="todo3"
+              type="checkbox"
+            />
+            <span className="leading-[1.2]">끝내주게 놀기</span>
           </label>
         </li>
       </ul>
