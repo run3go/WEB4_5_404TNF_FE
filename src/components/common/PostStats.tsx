5684cf700330442eed9e30981725fa69fcc5b591
ty <composedkim@naver.com>
2025-07-07 23:33:28 +0900

    💄Style: 좋아요, 댓글, 조회수 폰트 설정

--- src/components/common/PostStats.tsx
+++ src/components/common/PostStats.tsx
@@ -9,7 +9,7 @@ interface PostStats {
 export default function PostStats({ comment, like, views }: PostStats) {
   return (
     <>
-      <div className="flex gap-4">
+      <div className="flex gap-4 font-medium">
         <div className="flex items-center gap-2">
           <Icon width="16px" height="16px" left="-105px" top="-207px" />
           <p className="text-[14px]">{comment}</p>
