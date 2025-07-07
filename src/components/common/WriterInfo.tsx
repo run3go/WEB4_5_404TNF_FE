3eba1701a86562ec2d3f4c8a2c8da3dd7b71f6f9
ty <composedkim@naver.com>
2025-07-07 13:19:22 +0900

    💄Style: 작성자 정보 너비 fit으로 변경

--- src/components/common/WriterInfo.tsx
+++ src/components/common/WriterInfo.tsx
@@ -6,7 +6,7 @@ interface WriterInfo {
 export default function WriterInfo({ name, postedAt }: WriterInfo) {
   return (
     <>
-      <div className="flex items-center gap-4">
+      <div className="flex w-fit cursor-pointer items-center gap-4">
         <div className="h-[42px] w-[42px] rounded-full bg-gray-500"></div>
         <div className="text-[14px] font-medium">
           <p>{name}</p>
