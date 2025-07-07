09ebb24268b0d2e1532f29b03b16f99b735fd986
ty <composedkim@naver.com>
2025-07-08 01:01:50 +0900

    💄Style: 게시글 디테일 퍼블리싱

--- src/app/post/[category]/[postId]/page.tsx
+++ src/app/post/[category]/[postId]/page.tsx
@@ -1,12 +1,17 @@
-export default async function PostDetail({
-  params,
-}: {
-  params: Promise<{ postId: string }>;
-}) {
-  const { postId } = await params;
+import CommentInput from '@/components/post/CommentInput';
+import CommentList from '@/components/post/CommentList';
+import PostDetailCard from '@/components/post/PostDetailCard';
+
+export default async function PostDetail() {
   return (
     <>
-      <h1>{postId}</h1>
+      <div className="scrollbar-hidden h-[calc(100vh-176px)] w-[1548px] overflow-y-auto rounded-[50px] bg-[var(--color-background)] pr-[120px] pl-[88px]">
+        <div className="flex w-[1308px] flex-col gap-8 pt-8">
+          <PostDetailCard />
+          <CommentInput />
+          <CommentList />
+        </div>
+      </div>
     </>
   );
 }
