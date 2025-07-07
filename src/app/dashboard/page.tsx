6c419c840536de2e4d053fe60aa5d0e3952d4cb2
run3go <qkrwjdtn09@gmail.com>
2025-07-07 11:26:42 +0900

    💄 Style: 스크롤 생기는 부분 수정

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -37,37 +37,35 @@ export default async function Dashboard() {
   ];
 
   return (
-    <>
-      <div className="w-[1548px] bg-[var(--background)] p-12">
-        <div className="mb-13 flex items-center justify-between">
-          <h2 className="text-[22px] font-bold">
-            마음이는 평균보다 3kg 무거워요! 다이어트가 필요하니 간식을 줄여보는
-            건 어떨까요?
-          </h2>
-          <SelectBox options={options} width={110} footstep />
-        </div>
-        <div className="flex gap-12">
-          <div className="flex flex-col gap-12">
-            <ProfileCard />
-            <LineGraphCard title="몸무게" dataset={weightDataset} />
-            <LineGraphCard title="수면시간" dataset={sleepDataset} />
-          </div>
-          <div className="flex flex-col gap-12">
-            <div className="relative text-xl font-medium">
-              <Image src={speechBubble} alt="말풍선" width={558} height={98} />
-              <div className="absolute top-1/2 left-1/2 w-105 -translate-x-1/2 -translate-y-1/2">
-                요즘은 시원해서 산책할 때 기분이 아주아주 좋아요!
-              </div>
-            </div>
-            <div className="flex gap-12">
-              <FeedCard />
-              <TodoCard />
-            </div>
-            <NoteCard />
-          </div>
-          <WalkCard />
-        </div>
+    <div className="w-full rounded-[50px] bg-[var(--color-background)] px-12 py-5">
+      <div className="mb-10 flex items-center justify-between">
+        <h2 className="text-xl font-bold">
+          마음이는 평균보다 3kg 무거워요! 다이어트가 필요하니 간식을 줄여보는 건
+          어떨까요?
+        </h2>
+        <SelectBox options={options} width={110} footstep />
       </div>
-    </>
+      <div className="flex gap-12">
+        <div className="flex flex-col gap-12">
+          <ProfileCard />
+          <LineGraphCard title="몸무게" dataset={weightDataset} />
+          <LineGraphCard title="수면시간" dataset={sleepDataset} />
+        </div>
+        <div className="flex flex-col gap-12">
+          <div className="relative text-xl font-medium">
+            <Image src={speechBubble} alt="말풍선" width={558} height={98} />
+            <div className="absolute top-1/2 left-1/2 w-105 -translate-x-1/2 -translate-y-1/2">
+              요즘은 시원해서 산책할 때 기분이 아주아주 좋아요!
+            </div>
+          </div>
+          <div className="flex gap-12">
+            <FeedCard />
+            <TodoCard />
+          </div>
+          <NoteCard />
+        </div>
+        <WalkCard />
+      </div>
+    </div>
   );
 }
