87e85051a8fb781a8b717fb376d72940b66f1d26
ty <composedkim@naver.com>
2025-07-08 11:17:22 +0900

    💄Style: textarea 내용이 많아지면 입력창 커지도록 변경

--- src/components/post/CommentInput.tsx
+++ src/components/post/CommentInput.tsx
@@ -5,9 +5,13 @@ export default function CommentInput() {
   return (
     <>
       <div className="flex flex-col items-end gap-5">
-        <Card className="h-[120px] w-[1308px] p-5">
+        <Card className="min-h-[120px] w-[1308px] p-5">
           <textarea
-            className="scrollbar-hidden h-full w-full resize-none text-[18px] font-medium text-[#2B2926] placeholder-[#909090] focus:outline-none"
+            className="h-full w-full resize-none text-[18px] font-medium text-[#2B2926] placeholder-[#909090] focus:outline-none"
+            onInput={(e) => {
+              e.currentTarget.style.height = 'auto';
+              e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
+            }}
             placeholder="댓글을 작성해주세요."
           />
         </Card>
