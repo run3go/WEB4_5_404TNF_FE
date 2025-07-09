79680703e79968e12b1bce0afc744f65ec1b3092
run3go <qkrwjdtn09@gmail.com>
2025-07-09 14:17:46 +0900

    💄 Style: 헤더 / 메인 컨테이너 사이즈 조절

--- src/app/diary/create/page.tsx
+++ src/app/diary/create/page.tsx
@@ -4,8 +4,10 @@ import Calendar from '@/components/diary/Calendar';
 import FeedInput from '@/components/diary/create/FeedInput';
 import Note from '@/components/diary/create/Note';
 import SingleInput from '@/components/diary/create/SingleInput';
-import SnackInput from '@/components/diary/create/SnackInput';
 import WalkingInput from '@/components/diary/create/WalkingInput';
+
+import diary from '@/assets/images/diary.svg';
+import Image from 'next/image';
 import { useState } from 'react';
 
 export default function DiaryCreate() {
@@ -19,30 +21,29 @@ export default function DiaryCreate() {
   ];
 
   return (
-    <div className="flex h-[904px] w-[1548px] flex-col gap-12 px-19 py-8">
-      <div className="flex justify-between">
-        <div className="flex items-center gap-12">
-          <Calendar selected={selected} setSelected={setSelected} />
+    <div className="flex h-200 w-full flex-col gap-8 bg-[var(--color-background)] px-19 py-8">
+      <div className="self-end">
+        <SelectBox options={options} width="105px" footstep />
+      </div>
+      <div className="flex gap-14">
+        <div className="flex w-105 flex-col items-center gap-12">
+          <div className="flex">
+            <Image src={diary} alt="오늘의 멍멍일지를 적어보아요!" />
+            <Calendar selected={selected} setSelected={setSelected} />
+          </div>
           <div className="flex flex-col gap-5 text-lg">
             <SingleInput title="수면시간" id="sleep" />
             <SingleInput title="몸무게" id="weight" />
           </div>
         </div>
-        <div className="self-start">
-          <SelectBox
-            options={options}
-            width={105}
-            expendMenuWidth={20}
-            footstep
-          />
+        <div className="flex h-70 w-full flex-col gap-18">
+          <div className="flex">
+            <FeedInput />
+            <WalkingInput />
+          </div>
+          <Note />
         </div>
       </div>
-      <div className="flex h-70 w-full gap-18">
-        <FeedInput />
-        <WalkingInput />
-        <SnackInput />
-      </div>
-      <Note />
     </div>
   );
 }
