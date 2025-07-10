0d18ab33af0c73645464e7276fc62528dce64c86
ty <composedkim@naver.com>
2025-07-11 02:09:50 +0900

    💄Style: 이용약관 모바일 퍼블리싱

---
+++ src/app/(auth)/terms/page.tsx
@@ -0,0 +1,134 @@
+import Icon from '@/components/common/Icon';
+
+export default async function Terms() {
+  return (
+    <>
+      <div className="scrollbar-hidden w-screen max-w-[1548px] min-w-[375px] overflow-y-auto bg-[var(--color-background)] py-10 sm:w-[calc(100vw-276px)]">
+        <p className="mt-4 text-center text-[16px] font-bold">이용약관 동의</p>
+        <div className="scrollbar-hidden mx-6 mt-9 h-[280px] overflow-y-auto rounded-[12px] border border-[#FCC389] p-4 pl-5">
+          <p className="text-[12px] font-medium">
+            본 서비스는 멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티
+            플랫폼으로 회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본
+            서비스는 멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다. 본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다. 본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다. 본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.본 서비스는
+            멍멍일지가 제공하는 반려견 일상 기록 및 커뮤니티 플랫폼으로
+            회원가입을 통해 다양한 기능을 이용하실 수 있습니다.
+          </p>
+        </div>
+        <div className="mt-3 ml-8 flex flex-col gap-[9px]">
+          <label className="flex h-[20px] cursor-pointer items-center gap-2">
+            <input type="checkbox" className="peer hidden" />
+            <Icon
+              width="19px"
+              height="19px"
+              left="-286px"
+              top="-380px"
+              className="scale-95 peer-checked:hidden"
+            />
+            <Icon
+              width="19px"
+              height="19px"
+              left="-246px"
+              top="-380px"
+              className="hidden scale-95 peer-checked:block"
+            />
+            <p className="text-[12px] font-medium">
+              [필수] 위 약관에 동의합니다.
+            </p>
+          </label>
+          <label className="flex h-[20px] cursor-pointer items-center gap-2">
+            <input type="checkbox" className="peer hidden" />
+            <Icon
+              width="19px"
+              height="19px"
+              left="-286px"
+              top="-380px"
+              className="scale-95 peer-checked:hidden"
+            />
+            <Icon
+              width="19px"
+              height="19px"
+              left="-246px"
+              top="-380px"
+              className="hidden scale-95 peer-checked:block"
+            />
+            <p className="text-[12px] font-medium">
+              [필수] 개인정보 수집 및 이용에 동의합니다.
+            </p>
+          </label>
+          <label className="flex h-[20px] cursor-pointer items-center gap-2">
+            <input type="checkbox" className="peer hidden" />
+            <Icon
+              width="19px"
+              height="19px"
+              left="-286px"
+              top="-380px"
+              className="scale-95 peer-checked:hidden"
+            />
+            <Icon
+              width="19px"
+              height="19px"
+              left="-246px"
+              top="-380px"
+              className="hidden scale-95 peer-checked:block"
+            />
+            <p className="text-[12px] font-medium">
+              [선택] 광고성 정보 수신에 동의합니다.
+            </p>
+          </label>
+        </div>
+
+        <div className="mx-6">
+          <button
+            className="mt-[21vh] flex h-[40px] w-full cursor-pointer items-center justify-center rounded-[12px] bg-[#FFDBAB] text-[14px] disabled:bg-[#2B2926]/20 disabled:text-[#909090] sm:h-[56px]"
+            disabled
+          >
+            다음 단계로
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
