cea2b5acf11a2a963c27eecaa2c99f052d68e6d3
ty <composedkim@naver.com>
2025-07-11 18:37:10 +0900

    💄Style: 회원가입 반응형 퍼블리싱

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import SignupForm from '@/components/auth/signup/SignupForm';
 export default async function Signup() {
   return (
     <>
-      <div className="scrollbar-hidden h-[calc(100vh-73px)] w-screen max-w-[1548px] min-w-[375px] overflow-y-auto bg-[var(--color-background)] py-10 sm:h-[800px] sm:w-[calc(100vw-276px)] sm:py-5">
+      <div className="scrollbar-hidden h-[calc(100vh-72px)] w-screen max-w-[1548px] min-w-[375px] overflow-y-auto bg-[var(--color-background)] py-10 sm:h-full sm:w-full sm:py-0">
         <div className="flex justify-center font-medium text-[#2B2926] sm:text-[32px]">
           <p className="font-bold text-[#FF9526]">멍멍일지</p>
           <p>와 함께하는 즐거운 반려생활</p>
