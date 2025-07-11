6e29f2f70091a60796996888479356291c87c804
run3go <qkrwjdtn09@gmail.com>
2025-07-11 10:46:51 +0900

    💄 Style: 일정 페이지 반응형 구현

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,133 @@
-export default async function Schedule() {
+'use client';
+import alternative from '@/assets/images/alternative-image.svg';
+import Card from '@/components/common/Card';
+import Icon from '@/components/common/Icon';
+import CalendarNav from '@/components/diary/CalendarNav';
+import { ko } from 'date-fns/locale';
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+
+export default function Schedule() {
+  const [selected, setSelected] = useState<Date | undefined>();
+  const mainRef = useRef<HTMLElement | null>(null);
+
   return (
-    <>
-      <h1>Schedule</h1>
-    </>
+    <main
+      ref={mainRef}
+      className="h-[calc(100%-72px)] w-screen bg-[var(--color-background)] px-6 py-8"
+    >
+      <div className="aspect-square w-full rounded-xl bg-[var(--color-background)] px-2 shadow-[0_3px_8px_rgba(0,0,0,0.24)]">
+        <DayPicker
+          mode="single"
+          selected={selected}
+          onSelect={setSelected}
+          locale={ko}
+          showOutsideDays
+          modifiers={{
+            hasSchedule: [
+              new Date(2025, 6, 1),
+              new Date(2025, 6, 4),
+              new Date(2025, 6, 5),
+              new Date(2025, 6, 8),
+              new Date(2025, 6, 10),
+              new Date(2025, 6, 13),
+              new Date(2025, 6, 14),
+              new Date(2025, 6, 17),
+            ],
+          }}
+          modifiersClassNames={{
+            hasSchedule: 'has-schedule relative',
+          }}
+          classNames={{
+            month_caption:
+              'font-medium mb-2 bg-[var(--color-primary-500)] -mx-2 rounded-t-xl py-2',
+            months: 'w-full relative',
+            month_grid: 'w-full',
+            weekday: 'h-6 font-medium',
+            root: 'h-full text-xs text-center',
+            day: 'w-1/7 sm:h-6 h-[45px] ',
+            day_button: 'w-full h-full cursor-pointer calendar-day',
+            outside: 'text-[var(--color-grey)]',
+            selected: 'calendar-circle',
+          }}
+          components={{
+            Nav: CalendarNav,
+          }}
+        />
+      </div>
+      <Card className="mt-8 max-h-70 min-h-31 w-full text-sm">
+        <div className="mb-3 flex justify-between text-[var(--color-grey)]">
+          <span>일정 목록</span>
+          <span>2025. 7. 5</span>
+        </div>
+        <ul className="scrollbar-hidden flex max-h-55 flex-col items-center overflow-y-scroll">
+          <Image
+            className="mt-5"
+            src={alternative}
+            alt="대체 이미지"
+            width={40}
+            height={28}
+            priority
+          />
+          <span className="mt-2 mb-3 text-[var(--color-grey)]">
+            등록된 일정이 없습니다
+          </span>
+          {/* <li className="flex items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+            <span>할 일1</span>
+            <div className="flex gap-3">
+              <Icon width="14px" height="14px" left="-225px" top="-168px" />
+              <Icon width="14px" height="14px" left="-266px" top="-167px" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+            <span>할 일2</span>
+            <div className="flex gap-3">
+              <Icon width="14px" height="14px" left="-225px" top="-168px" />
+              <Icon width="14px" height="14px" left="-266px" top="-167px" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+            <span>할 일3</span>
+            <div className="flex gap-3">
+              <Icon width="14px" height="14px" left="-225px" top="-168px" />
+              <Icon width="14px" height="14px" left="-266px" top="-167px" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+            <span>할 일3</span>
+            <div className="flex gap-3">
+              <Icon width="14px" height="14px" left="-225px" top="-168px" />
+              <Icon width="14px" height="14px" left="-266px" top="-167px" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+            <span>할 일3</span>
+            <div className="flex gap-3">
+              <Icon width="14px" height="14px" left="-225px" top="-168px" />
+              <Icon width="14px" height="14px" left="-266px" top="-167px" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+            <span>할 일3</span>
+            <div className="flex gap-3">
+              <Icon width="14px" height="14px" left="-225px" top="-168px" />
+              <Icon width="14px" height="14px" left="-266px" top="-167px" />
+            </div>
+          </li>
+          <li className="flex items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+            <span>할 일3</span>
+            <div className="flex gap-3">
+              <Icon width="14px" height="14px" left="-225px" top="-168px" />
+              <Icon width="14px" height="14px" left="-266px" top="-167px" />
+            </div>
+          </li> */}
+        </ul>
+      </Card>
+      <div className="fixed right-4 bottom-4 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[var(--color-primary-300)] sm:hidden">
+        <Icon width="20px" height="20px" left="-266px" top="-75px" />
+      </div>
+      {/* {mainRef.current && createPortal(<AddSchedule />, mainRef.current)} */}
+    </main>
   );
 }
---
+++ src/components/schedule/AddSchedule.tsx
@@ -0,0 +1,75 @@
+import Button from '../common/Button';
+import Icon from '../common/Icon';
+import SelectBox from '../common/SelectBox';
+
+export default function AddSchedule() {
+  const options = [
+    { value: '이마음', label: '이마음' },
+    { value: '이구름', label: '이구름' },
+    { value: '이솜', label: '이솜' },
+  ];
+
+  const cycles = [
+    { value: 'WEEK', label: '매주' },
+    { value: 'ONE_MONTH', label: '매달' },
+    { value: 'THREE_MONTH', label: '매분기' },
+    { value: 'SIX_MONTH', label: '반년' },
+    { value: 'YEAR', label: '매년' },
+  ];
+  return (
+    <>
+      <div className="absolute inset-0 z-101 bg-[var(--color-black)] opacity-50" />
+      <div className="absolute top-1/2 left-1/2 z-102 h-[348px] w-4/5 max-w-250 -translate-x-1/2 -translate-y-1/2 rounded-[30px] border-4 border-[var(--color-primary-200)] bg-[var(--color-background)] p-5">
+        <form className="relative flex h-full flex-col gap-4 text-sm">
+          <div className="mb-1 flex w-full items-center justify-between">
+            <h2 className="text-base font-extrabold">일정추가</h2>
+            <Icon width="12px" height="12px" left="-72px" top="-126px" />
+          </div>
+          <div className="flex items-center justify-between">
+            <label className="block w-13" htmlFor="todo">
+              할 일
+            </label>
+            <input
+              className="input-style w-full px-3 py-[11px]"
+              type="text"
+              placeholder="할 일을 입력하세요"
+            />
+          </div>
+          <div className="flex">
+            <span className="inline-block w-[57px]">날짜</span>
+            <span>2025. 7. 6</span>
+            <Icon
+              className="ml-[14px] scale-80"
+              width="20px"
+              height="20px"
+              left="-188px"
+              top="-123px"
+            />
+          </div>
+          <div className="flex items-center">
+            <span className="inline-block w-[57px]">강아지</span>
+            <SelectBox options={options} width="90px" footstep />
+          </div>
+          <div className="flex items-center">
+            <span className="inline-block w-[57px]">반복</span>
+            <SelectBox options={cycles} width="80px" isCenter />
+          </div>
+          <div className="flex">
+            <span className="mr-4">반복 종료일</span>
+            <span>2025. 7. 6</span>
+            <Icon
+              className="ml-[14px] scale-80"
+              width="20px"
+              height="20px"
+              left="-188px"
+              top="-123px"
+            />
+          </div>
+          <Button className="absolute bottom-0 h-10 w-25 self-center text-sm">
+            저장하기
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+}
