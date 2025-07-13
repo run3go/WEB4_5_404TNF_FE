bda0fab78b3f6ae0c2cec8302eb2a70daaf9db7a
YooJung Kwon <best106yj@naver.com>
2025-07-13 23:05:10 +0900

    💄 Style: 404페이지 이미지 변경, 폴더 구조 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import Button from '@/components/common/Button';
 export default function NotFound() {
   return (
     <>
-      <div className="flex h-full w-full flex-col items-center gap-12 bg-[url('/images/404.svg')] bg-cover pt-45">
-        <div className="flex cursor-default flex-col items-center gap-3 text-2xl font-bold sm:text-[40px]">
+      <div className="flex h-screen w-screen flex-col items-center gap-12 bg-[url('/images/404-mobile.png')] bg-cover bg-center bg-no-repeat pt-45 sm:bg-[url('/images/404.png')]">
+        <div className="flex cursor-default flex-col items-center gap-3 text-xl font-bold sm:text-[40px]">
           <h1 className="text-[var(--color-primary-500)]">404!</h1>
           <h1>냄새를 따라왔는데... 여긴 아닌가봐요.</h1>
           <h1>다시 발자국을 따라가볼까요?</h1>
