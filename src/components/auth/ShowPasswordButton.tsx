38281b574c91cf3cae7c742d481f1ad2a70830e5
ty <composedkim@naver.com>
2025-07-15 10:36:12 +0900

    ✨ feat: 비밀번호 보는 아이콘 컴포넌트 생성

---
+++ src/components/auth/ShowPasswordButton.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Icon from '@/components/common/Icon';
+
+interface PasswordToggleButtonProps {
+  isVisible: boolean;
+  onClick: () => void;
+}
+
+export default function PasswordToggleButton({
+  isVisible,
+  onClick,
+}: PasswordToggleButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="absolute top-[32px] right-3 -translate-y-1/2 transform text-gray-400 hover:text-gray-300 sm:top-[38px]"
+    >
+      {isVisible ? (
+        <Icon
+          width="16px"
+          height="16px"
+          left="-186px"
+          top="-167px"
+          className="scale-75 skew-x-12 cursor-pointer sm:scale-100"
+        />
+      ) : (
+        <Icon
+          width="16px"
+          height="16px"
+          left="-186px"
+          top="-167px"
+          className="scale-75 cursor-pointer sm:scale-100"
+        />
+      )}
+    </button>
+  );
+}
