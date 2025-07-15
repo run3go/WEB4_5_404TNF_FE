1c4a8a3482f517b4438eac967b09c1acf403ae68
mjlee <leeminji9938@naver.com>
2025-07-15 14:18:50 +0900

    🔧 Chore: '전체 날짜' placeholder props 추가

--- src/components/common/DateInput.tsx
+++ src/components/common/DateInput.tsx
@@ -10,15 +10,25 @@ export default function DateInput({
   className,
   selected,
   setSelected,
+  showAllDate = false,
+  placeholder = '전체 날짜',
 }: {
   className: string;
   selected: Date | undefined;
   setSelected: Dispatch<SetStateAction<Date | undefined>>;
+  showAllDate?: boolean;
+  placeholder?: string;
 }) {
   const [isDateInputOpen, setIsDateInputOpen] = useState(false);
   const inputRef = useRef<HTMLDivElement>(null);
 
   const today = new Date();
+
+  const displayDateText = selected
+    ? formatDate(selected, 'yyyy. MM. dd')
+    : showAllDate
+      ? placeholder
+      : formatDate(today, 'yyyy. MM. dd');
 
   const handleSelectDate = (date: Date | undefined) => {
     setIsDateInputOpen(false);
@@ -42,9 +52,7 @@ export default function DateInput({
         )}
         onClick={() => setIsDateInputOpen(true)}
       >
-        {selected
-          ? formatDate(selected, 'yyyy. MM. dd')
-          : formatDate(today, 'yyyy. MM. dd')}
+        {displayDateText}
         <Icon
           className="scale-90"
           width="20px"
