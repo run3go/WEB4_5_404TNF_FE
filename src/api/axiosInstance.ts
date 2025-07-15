84b7683bd009789f16182ff6ef33871684eb468e
ty <composedkim@naver.com>
2025-07-15 15:05:25 +0900

    🔧Chore: baseURL 변경

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://mungnote-172598302113.asia-northeast3.run.app',
+  baseURL: 'https://mungdiary-172598302113.asia-northeast3.run.app',
   withCredentials: true,
 });
