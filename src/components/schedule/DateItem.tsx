7fdd5a16e1b73c0e2baea46d8b4ee3af522252c7
YooJung Kwon <best106yj@naver.com>
2025-07-15 09:51:15 +0900

    💄 Style: 캘린더 날짜 영역 ui 수정

--- src/components/schedule/DateItem.tsx
+++ src/components/schedule/DateItem.tsx
@@ -3,11 +3,13 @@ import { getDaysInMonth } from 'date-fns';
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
 import TodoList from './TodoList';
+import AddSchedule from './AddSchedule';
 
 export default function DateItem({
   date,
   targetMonth,
   schedules,
+  isToday,
 }: {
   date: number;
   targetMonth: Date;
@@ -17,6 +19,7 @@ export default function DateItem({
     name: string;
     isDone: boolean;
   }[];
+  isToday: boolean;
 }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -29,13 +32,24 @@ export default function DateItem({
     <>
       <div
         key={date}
-        className="h-24 basis-1/7 cursor-pointer border-b border-[var(--color-primary-200)] p-3 transition-colors duration-200 ease-in hover:bg-[var(--color-primary-100)]"
-        onClick={() => setIsModalOpen(true)}
+        onClick={() => {
+          if (date > 0 && date <= endOfMonth) {
+            setIsModalOpen(true);
+          }
+        }}
+        className={`h-24 basis-1/7 border-b border-[var(--color-primary-200)] p-3 ${date > 0 && date <= endOfMonth ? 'cursor-pointer transition-colors duration-200 ease-in hover:bg-[var(--color-primary-100)]' : ''}`}
       >
         {date > 0 && date <= endOfMonth && (
           <>
             <div className="flex justify-between">
-              <span>{date}</span>
+              <div className="relative flex h-6 w-6 items-center justify-center">
+                {isToday && (
+                  <div
+                    className={`absolute inset-0 -top-1.5 ${String(date).length === 1 ? '-left-1' : '-left-0.5'} size-8 rounded-full bg-[var(--color-primary-500)]`}
+                  />
+                )}
+                <span className="z-100">{date}</span>
+              </div>
               {schedules.length > 2 && (
                 <span className="text-sm text-[var(--color-grey)]">+ more</span>
               )}
@@ -56,7 +70,19 @@ export default function DateItem({
       </div>
       {isModalOpen &&
         createPortal(
-          <TodoList type="modal" closeModal={closeModal} />,
+          schedules.length === 0 ? (
+            <AddSchedule
+              closeModal={closeModal}
+              isStart={true}
+              isEdit={false}
+            />
+          ) : (
+            <TodoList
+              type="modal"
+              closeModal={closeModal}
+              schedules={schedules}
+            />
+          ),
           document.body,
         )}
     </>
