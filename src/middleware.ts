2e9514c5b29c71ce5a1b805d50b5b30c84eb2225
ty <composedkim@naver.com>
2025-07-17 01:14:26 +0900

    ✨ feat: 비로그인  접근 제어 추가

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('userId')?.value;
+  console.log(token);
+  const allowedPaths = [
+    '/post',
+    '/signup',
+    '/terms',
+    '/guide',
+    '/login',
+    '/adminlogin',
+  ];
+
+  const isAllowed = allowedPaths.some((path) =>
+    req.nextUrl.pathname.startsWith(path),
+  );
+
+  const isRootPath = req.nextUrl.pathname === '/';
+
+  if (!token && !isAllowed && !isRootPath) {
+    const loginUrl = req.nextUrl.clone();
+    loginUrl.pathname = '/login';
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
