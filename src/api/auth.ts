aac6f41320819877887917ecd87f1af1e9335e26
ty <composedkim@naver.com>
2025-07-16 11:09:36 +0900

    ✨ feat: 로그아웃, 프로필 정보 api 설정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -8,8 +8,39 @@ export const login = async (email: string, password: string) => {
     },
   );
 
+  const data = await res.json();
   if (!res.ok) {
-    const data = await res.json();
     throw new Error(data.message || '로그인 실패');
+  }
+
+  return data;
+};
+
+export const logout = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/v1/logout`,
+    {
+      method: 'POST',
+    },
+  );
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || '로그인 실패');
+  }
+
+  return data;
+};
+
+export const getUserProfile = async (userId: string) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/profile/v1/${userId}`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return;
   }
 };
