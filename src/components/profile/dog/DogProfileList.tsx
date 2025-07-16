59ecca6f7c5d5d920808e2b5ed0068dc5ca70a57
run3go <qkrwjdtn09@gmail.com>
2025-07-16 15:15:02 +0900

    🚀 Perform: 반려견 프로필 카드 프리렌더링

--- src/components/profile/dog/DogProfileList.tsx
+++ src/components/profile/dog/DogProfileList.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Card from '@/components/common/Card';
 import { useProfileStore } from '@/stores/profileStore';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { useMediaQuery } from 'react-responsive';
 import 'swiper/css';
@@ -14,7 +14,11 @@ import DogProfileCard from './DogProfileCard';
 import DogProfileEdit from './DogProfileEdit';
 import RegistCard from './RegistCard';
 
-export default function DogProfileList() {
+export default function DogProfileList({
+  profileData,
+}: {
+  profileData: PetProfile[];
+}) {
   const isMobile = useMediaQuery({
     query: '(max-width: 767px)',
   });
@@ -22,6 +26,7 @@ export default function DogProfileList() {
   const petProfiles = useProfileStore((state) => state.petProfiles);
   const togglePage = useProfileStore((state) => state.toggleEditingPetProfile);
 
+  const [localProfiles, setLocalProfiles] = useState<PetProfile[]>(profileData);
   const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
   const prevRef = useRef(null);
@@ -31,6 +36,10 @@ export default function DogProfileList() {
     setIsProfileModalOpen((state) => !state);
   };
 
+  useEffect(() => {
+    setLocalProfiles(petProfiles);
+  }, [petProfiles]);
+
   return (
     <div className="mb-20 w-full">
       <h2 className="text-sm text-[var(--color-primary-500)] sm:text-xl">
@@ -38,8 +47,8 @@ export default function DogProfileList() {
       </h2>
       {isMobile ? (
         <div className="mt-6 flex flex-col gap-6">
-          {petProfiles &&
-            petProfiles.map((profile, index) => (
+          {localProfiles &&
+            localProfiles.map((profile, index) => (
               <DogProfileCard
                 key={index}
                 togglePage={togglePage}
@@ -115,8 +124,8 @@ export default function DogProfileList() {
                 }
               }}
             >
-              {petProfiles &&
-                petProfiles.map((profile, index) => (
+              {localProfiles &&
+                localProfiles.map((profile, index) => (
                   <SwiperSlide key={index} className="!w-[598px]">
                     <DogProfileCard profile={profile} />
                   </SwiperSlide>
