d01259599a256f00826fbabe8c11a28c0cf347ff
ty <composedkim@naver.com>
2025-07-16 11:08:18 +0900

    ✨ feat: user 타입 설정

---
+++ src/types/user.d.ts
@@ -0,0 +1,20 @@
+type UserProfile = {
+  userId: number;
+  email: string;
+  state: boolean;
+  name: string;
+  nickname: string;
+  role: 'ROLE_USER' | 'ROLE_ADMIN';
+  password: null;
+  provider: 'GOOGLE' | 'KAKAO' | 'NAVER' | 'LOCAL';
+  userImg: null;
+};
+
+interface User {
+  userId: number;
+  email: string;
+  name: string;
+  nickname: string;
+  provider: string;
+  userImg: string | null;
+}
