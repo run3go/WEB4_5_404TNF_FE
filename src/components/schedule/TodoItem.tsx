75dc62a925cc2efd9bb3cefdb6bac01b41591207
YooJung Kwon <best106yj@naver.com>
2025-07-16 14:21:50 +0900

    💄 Style: 일정 삭제 팝업메뉴 추가

--- src/components/schedule/TodoItem.tsx
+++ src/components/schedule/TodoItem.tsx
@@ -2,25 +2,45 @@
 import { useState } from 'react';
 import Icon from '../common/Icon';
 import AddSchedule from './AddSchedule';
+import PopupMenu from '../common/PopupMenu';
 // import Image from 'next/image';
 // import pet from '../../assets/images/dog_img.png';
 
-export default function TodoItem({ name, id }: { name: string; id?: number }) {
+export default function TodoItem({
+  name,
+  schedule,
+}: {
+  name?: string;
+  schedule?: Schedule;
+}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  const deleteTodo = () => {
+  const handleSelect = (label: string) => {
+    if (label === '이 일정만 삭제') {
+      console.log('이 일정만 삭제');
+      // deleteTodo(false);
+    } else {
+      console.log('반복 일정 전체 삭제');
+      // deleteTodo(true);
+    }
+
+    setIsMenuOpen(false);
+  };
+
+  const deleteTodo = (cycleLink: boolean) => {
     // 일정 삭제 api
     // toast
   };
 
   return (
     <>
-      <li className="flex w-full items-center justify-between border-b border-[var(--color-primary-300)] p-3">
+      <li className="flex w-full cursor-default items-center justify-between border-b border-[var(--color-primary-300)] p-3">
         <div className="flex items-center justify-center">
-          <span className="mr-2 rounded-[8px] bg-[var(--color-primary-300)] px-2 py-1 text-sm">
+          <div className="mr-2 max-w-[80px] truncate rounded-[8px] bg-[var(--color-primary-300)] px-2 py-1 text-sm">
             {/* <Image
               src={pet}
               // className="h-4 w-4 rounded-full"
@@ -28,12 +48,14 @@ export default function TodoItem({ name, id }: { name: string; id?: number }) {
               height={16}
               alt="강아지 프로필"
             /> */}
-            이마음
-          </span>
+            {schedule?.petName}
+          </div>
 
-          {name}
+          <span className="max-w-[340px] truncate">
+            {name ? name : schedule?.name}
+          </span>
         </div>
-        <div className="flex gap-5">
+        <div className="relative flex gap-5">
           <Icon
             className="cursor-pointer"
             onClick={() => setIsModalOpen(true)}
@@ -43,13 +65,27 @@ export default function TodoItem({ name, id }: { name: string; id?: number }) {
             top="-168px"
           />
           <Icon
-            onClick={deleteTodo}
+            onClick={() => setIsMenuOpen((prev) => !prev)}
             className="cursor-pointer"
             width="14px"
             height="14px"
             left="-266px"
             top="-167px"
           />
+
+          {isMenuOpen && (
+            <div className="absolute top-3 left-13 z-50">
+              <PopupMenu
+                options={[
+                  { id: '0', label: '이 일정만 삭제', type: 'delete' },
+                  { id: '1', label: '반복 일정 전체 삭제', type: 'delete' },
+                ]}
+                onSelect={handleSelect}
+                onClose={() => setIsMenuOpen(false)}
+                className={'text-[var(--color-black)]'}
+              />
+            </div>
+          )}
         </div>
       </li>
       {isModalOpen && (
