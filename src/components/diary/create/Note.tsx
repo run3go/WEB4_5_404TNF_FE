5b95a59d662260866cd9753c5aa18ea11c4054f0
mjlee <leeminji9938@naver.com>
2025-07-17 14:43:54 +0900

    💄 Style: 관찰노트 textarea 스크롤바 제거

--- src/components/diary/create/Note.tsx
+++ src/components/diary/create/Note.tsx
@@ -8,7 +8,7 @@ export default function Note({ value, onChange }: Props) {
   return (
     <DiaryCard className="h-full" title="관찰노트">
       <textarea
-        className="h-30 w-full resize-none px-1 py-1 focus:outline-none sm:h-full"
+        className="scrollbar-hidden h-30 w-full resize-none px-1 py-1 focus:outline-none sm:h-full"
         placeholder="댕댕이의 하루를 관찰하여 내용을 입력해주세요"
         value={value}
         onChange={(e) => onChange(e.target.value)}
