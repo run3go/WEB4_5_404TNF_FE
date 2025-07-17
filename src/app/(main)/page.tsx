c7b47e4ae8af88043f2e98e667fe60d21c3f4c79
ty <composedkim@naver.com>
2025-07-17 11:30:02 +0900

    ✨ feat: 액세스토큰 확인

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ import LandingMockup from '@/components/home/LandingMockup';
 import { cookies } from 'next/headers';
 export default async function Main() {
   const cookieStore = cookies();
-  const userId = (await cookieStore).get('userId')?.value;
+  const userId = (await cookieStore).get('ACCESS_TOKEN')?.value;
   console.log(userId);
   return (
     <>
