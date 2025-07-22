674d24702b03137dcc896161b9d93cf3a0fca49f
ty <composedkim@naver.com>
2025-07-22 23:10:33 +0900

    🔧Chore: 상수 파일로 변경

---
+++ src/assets/data/post.ts
@@ -0,0 +1,12 @@
+export const SORT = [
+  { value: 'DATE', label: '최신순' },
+  { value: 'LIKE', label: '좋아요순' },
+  { value: 'VIEW', label: '조회수순' },
+];
+
+export const SEARCH_LIST = [
+  { value: 'TITLE_CONTENT', label: '제목 + 내용' },
+  { value: 'TITLE', label: '제목' },
+  { value: 'CONTENT', label: '내용' },
+  { value: 'AUTHOR', label: '작성자' },
+];
