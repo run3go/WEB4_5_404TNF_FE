279a97aff830baf19d270e88ede1ccf0412448cf
ty <composedkim@naver.com>
2025-07-27 17:47:08 +0900

    🔧Chore: api endpoint env로 변경

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -46,12 +46,9 @@ export const logout = async () => {
 
 export const getUserProfile = async (userId: string) => {
   try {
-    const response = await fetch(
-      `https://mungdiary-172598302113.asia-northeast3.run.app/api/profile/v1/users/${userId}`,
-      {
-        credentials: 'include',
-      },
-    );
+    const response = await fetch(`${baseURL}/api/profile/v1/users/${userId}`, {
+      credentials: 'include',
+    });
     const data = await response.json();
     return data;
   } catch (err) {
@@ -61,15 +58,12 @@ export const getUserProfile = async (userId: string) => {
 };
 
 export const checkEmailDuplicate = async (email: string) => {
-  const res = await fetch(
-    `https://mungdiary-172598302113.asia-northeast3.run.app/api/auth/v1/check-email?email=${email}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+  const res = await fetch(`${baseURL}/api/auth/v1/check-email?email=${email}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+  });
 
   if (!res.ok) {
     const data = await res.json();
@@ -84,14 +78,11 @@ export const checkEmailDuplicate = async (email: string) => {
 };
 
 export const sendEmailVerification = async (email: string) => {
-  const res = await fetch(
-    'https://mungdiary-172598302113.asia-northeast3.run.app/api/auth/v1/email-verifications',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email }),
-    },
-  );
+  const res = await fetch(`${baseURL}/api/auth/v1/email-verifications`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email }),
+  });
   if (!res.ok) {
     const data = await res.json();
     throw new Error(data.message || '이메일 인증코드 보내기 실패');
@@ -108,14 +99,11 @@ export const verifyEmailCode = async (
   email: string,
   verificationCode: string,
 ) => {
-  const res = await fetch(
-    'https://mungdiary-172598302113.asia-northeast3.run.app/api/auth/v1/email-verifications/verify',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, verificationCode }),
-    },
-  );
+  const res = await fetch(`${baseURL}/api/auth/v1/email-verifications/verify`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, verificationCode }),
+  });
 
   if (!res.ok) {
     const data = await res.json();
@@ -125,7 +113,7 @@ export const verifyEmailCode = async (
 
 export const checkNicknameDuplicate = async (nickname: string) => {
   const res = await fetch(
-    `https://mungdiary-172598302113.asia-northeast3.run.app/api/auth/v1/check-nickname?nickname=${nickname}`,
+    `${baseURL}/api/auth/v1/check-nickname?nickname=${nickname}`,
     {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
@@ -144,14 +132,11 @@ export const register = async (formData: {
   email: string;
   password: string;
 }) => {
-  const res = await fetch(
-    'https://mungdiary-172598302113.asia-northeast3.run.app/api/auth/v2/register',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData),
-    },
-  );
+  const res = await fetch(`${baseURL}/api/auth/v2/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+  });
 
   if (!res.ok) {
     const data = await res.json();
