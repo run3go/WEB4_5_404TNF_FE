60626bb4fb4c4d298ea2f88a1cec2e717a4c0d4f
run3go <qkrwjdtn09@gmail.com>
2025-07-28 10:51:14 +0900

    💄 Style: 챗봇 아이콘 수정

--- src/components/chatbot/ChatbotIcon.tsx
+++ src/components/chatbot/ChatbotIcon.tsx
@@ -1,8 +1,7 @@
 'use client';
-import dogImage from '@/assets/images/default-profile.svg';
 import { useAuthStore } from '@/stores/authStoe';
-import Image from 'next/image';
 import { useState } from 'react';
+import Icon from '../common/Icon';
 import ChatbotWrapper from './ChatbotWrapper';
 
 export default function ChatbotIcon() {
@@ -18,19 +17,12 @@ export default function ChatbotIcon() {
       <>
         <div className="absolute right-10 bottom-10 flex h-13 w-13 items-center justify-center rounded-full bg-[var(--color-background)] transition-colors">
           <div
-            className="cursor-pointer"
+            className="flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full bg-[var(--color-primary-300)] hover:bg-[var(--color-primary-500)]"
             onClick={() => {
               setIsModalOpen(true);
             }}
           >
-            <Image
-              className="h-13 w-13"
-              src={dogImage}
-              alt="챗봇"
-              width={52}
-              height={52}
-              priority
-            />
+            <Icon width="24px" height="21px" left="-66px" top="-447px" />
           </div>
           {isModalOpen && <ChatbotWrapper onClose={handleClose} />}
         </div>
