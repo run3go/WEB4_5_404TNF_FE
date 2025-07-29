76fe9bc9e0bacf4f6b47a6ef09dfc2a6b0bc6df0
ty <composedkim@naver.com>
2025-07-30 08:54:59 +0900

    🔧Chore: console.log 삭제

--- src/app/(main)/post/[category]/[postId]/page.tsx
+++ src/app/(main)/post/[category]/[postId]/page.tsx
@@ -1,4 +1,4 @@
-import { getLikeCount, getPostDetail } from '@/api/post';
+import { getPostDetail } from '@/api/post';
 import PostDetailWrapper from '@/components/post/PostDetailWrapper';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
@@ -27,9 +27,6 @@ export default async function PostDetail({
     notFound();
   }
   const data = await getPostDetail(Number(postId));
-  const like = await getLikeCount(Number(postId));
-
-  console.log(like);
 
   return (
     <>
